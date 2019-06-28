a35e5f9e30752b06b90c2c20086bd4b27cfa6a19
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-06-28 10:03:18 +0400

    refactor mongoose to eliminate deprecation error

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
+mongoose.set("useCreateIndex", true);
 
 const app = express();
 require("dotenv").config();
