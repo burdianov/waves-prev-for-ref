b12b2bdec1c15366dcb0fc9f02750f30ed7540fe
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-06-28 23:22:56 +0400

    brand model and routes

---
+++ server/middleware/admin.js
@@ -0,0 +1,9 @@
+const { Brand } = require("./../models/brand");
+
+const admin = (req, res, next) => {
+  if (req.user.role === 0) return res.send("You are not allowed, get out now!");
+
+  next();
+};
+
+module.exports = { admin };
